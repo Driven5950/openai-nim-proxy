92089eedc9428ddf1e0e71f2c9a980a74620eba1
Driven5950 <126188753+Driven5950@users.noreply.github.com>
2026-03-01 18:34:33 -0800

    Create models.js

---
+++ api/models.js
@@ -0,0 +1,15 @@
+const MODEL_MAPPING = {
+  'minimaxai/minimax-m2.5',
+  'qwen/qwen3.5-397b-a17b',
+  'z-ai/glm5',
+  'minimaxai/minimax-m2.1',
+  'stepfun-ai/step-3.5-flash',
+  'moonshotai/kimi-k2.5',
+  'z-ai/glm4.7',
+  'deepseek-ai/deepseek-v3.2',
+  'deepseek-ai/deepseek-v3.1-terminus',
+  'qwen/qwen3-next-80b-a3b-instruct',
+  'mistralai/devstral-2-123b-instruct-2512',
+  'mistralai/mistral-large-3-675b-instruct-2512',
+  'qwen/qwen3-coder-480b-a35b-instruct',
+};
