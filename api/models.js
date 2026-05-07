252c201f2aeda2abd320a48c7cbf8f6d2eb9f06e
Driven5950 <126188753+Driven5950@users.noreply.github.com>
2026-05-06 17:10:33 -0700

    Update models.js

--- api/models.js
+++ api/models.js
@@ -6,7 +6,7 @@ const MODEL_MAPPING = {
   'minimaxai/minimax-m2.1': 'minimaxai/minimax-m2.1',
   'stepfun-ai/step-3.5-flash': 'stepfun-ai/step-3.5-flash',
   'moonshotai/kimi-k2.5': 'moonshotai/kimi-k2.5',
-  'z-ai/glm-4.7': 'z-ai/glm-4.7',
+  'z-ai/glm4.7': 'z-ai/glm4.7',
   'deepseek-ai/deepseek-v3.2': 'deepseek-ai/deepseek-v3.2',
   'deepseek-ai/deepseek-v3.1-terminus': 'deepseek-ai/deepseek-v3.1-terminus',
   'qwen/qwen3-next-80b-a3b-instruct': 'qwen/qwen3-next-80b-a3b-instruct',
@@ -17,7 +17,7 @@ const MODEL_MAPPING = {
   'deepseek-ai/deepseek-v4-pro': 'deepseek-ai/deepseek-v4-pro',
   'deepseek-ai/deepseek-v4-flash': 'deepseek-ai/deepseek-v4-flash',
   'mistralai/mistral-medium-3.5-128b': 'mistralai/mistral-medium-3.5-128b',
-  'z-ai/glm-5.1': 'z-ai/glm-5.1',
+  'z-ai/glm5.1': 'z-ai/glm5.1',
   'qwen/qwen3.5-122b-a10b': 'qwen/qwen3.5-122b-a10b',
   'nvidia/nemotron-3-super-120b-a12b': 'nvidia/nemotron-3-super-120b-a12b'
 };
